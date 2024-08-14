05e0b9a1b6107068e97c1db44bc0e45c6ab2bbe5
kushaalrajiv <kushaalrajiv@gmail.com>
2024-08-14 10:35:15 +0530

    using BFS method - medium

---
+++ completeness_binarytree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isCompleteTree(self, root: Optional[TreeNode]) -> bool:
+        queue=deque()
+        queue.append(root)
+        end=False
+        while queue:
+            node=queue.popleft()
+            if not node:
+                end=True
+            else:
+                if end:
+                    return False
+                queue.append(node.left)
+                queue.append(node.right)
+        return True
+
+        
