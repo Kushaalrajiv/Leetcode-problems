433144abcbffad45660ae915e62c156cbac1994c
kushaalrajiv <kushaalrajiv@gmail.com>
2024-08-25 15:42:37 +0530

    BFS

---
+++ reverse_level_order_binarytree.py
@@ -0,0 +1,28 @@
+'''
+class Node:
+    def __init__(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+def reverseLevelOrder(root):
+    # code here
+    if not root:
+        return []
+    queue=deque()
+    queue.append(root)
+    res=[]
+    while queue:
+        length=len(queue)
+        levels=[]
+        i=0
+        while i<length:
+            node=queue.popleft()
+            levels.append(node.data)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+            i+=1
+        res.append(levels)
+    return [val for j in reversed(res) for val in j]
